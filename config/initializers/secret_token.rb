7ac507feb3496f3ae51ef3a52d2cf6346b108941
vagrant <vagrant@precise64.(none)>
2014-04-01 05:00:34 +0000

    Uploading the project out of the box.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpreebootstrapWeb::Application.config.secret_key_base = 'd385d0cc70112511bfc61bd4e581127d100e5e8ca241034d15d91542296a741a1071e87f98ea102ce203d53203ea044e239f21fccd7efb5fe0a82f919ad7b5f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpreebootstrapWeb::Application.config.session_store :cookie_store, key: '_spreebootstrap-web_session'
